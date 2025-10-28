43e6b5229b618d5535d6df4e6cbf72d53db2bb79
Ji Jin Yan <fengxinzeisao@gmail.com>
2025-10-28 16:56:47 +0100

    stop words deleted from index, words 'toldalektalanitva'. global index builded

--- termnyelv/index/indexSearch.py
+++ termnyelv/index/indexSearch.py
@@ -1,15 +1,37 @@
 import re
+import nltk
+from nltk.stem import SnowballStemmer
+from simplemma import lemmatize
+from nltk.corpus import stopwords
 
-def find_index(pagecontent):
+#megkeresi a pageben levo szavak szamat
+def find_index_number(pagecontent):
     # ebben taroljuk a szavakat, es helyet
     index = {}
+
     text = pagecontent['text']
+
+    #toldalektalaniti a szavakat
+    stemmer = SnowballStemmer('hungarian')
+
+    #stop szavak szuro
+    stopword = set(stopwords.words('hungarian'))
     #szetvalaszt megkeres
     for tok in [i for i in re.split(r'(\d+|\W+)', text) if i]:
+
         #kis nagy betu ne legyen kulonbseg
         tok = tok.lower()
+
+        #megvalositja a toldalektalanitast
+        tok = stemmer.stem(tok)
+
+        #stop szo kiszedes az indexbol
+        if tok in stopword:
+            continue
+
         if len(tok) < 2:
             continue
+
         #elso talalat a szora
         if not tok in index:
             index[tok] = 1
@@ -22,7 +44,38 @@ def find_index(pagecontent):
 
    #1, 'legjobb' talalat kell llmn-ek -->> megszamolni hanyszor fordul elo ebben az oldalon
 #mo: azt majd return
-   #2, a szavak nincsenek toldalektalanitva (mennyire fontos???)
-   #3, toldalel talanitas jobban mukodne, keresesben es majd llm-nek elott  is jobb igy keresni
-   #4, felesleges toldalek szo eltavolitas
-   #FK MOODLE IS DEAD :DDD
+   #2, a szavak nincsenek toldalektalanitva (pipa
+   #3, toldalel talanitas jobban mukodne, keresesben es majd llm-nek elott  is jobb igy keresni (pipa
+   #4, felesleges toldalek szo eltavolitas (pipa
+
+def find_index_words(pagecontent):
+
+    index = set()
+
+    text = pagecontent['text']
+
+    # toldalektalaniti a szavakat
+    stemmer = SnowballStemmer('hungarian')
+
+    # stop szavak szuro
+    stopword = set(stopwords.words('hungarian'))
+    # szetvalaszt megkeres
+    for tok in [i for i in re.split(r'(\d+|\W+)', text) if i]:
+
+        # kis nagy betu ne legyen kulonbseg
+        tok = tok.lower()
+
+        # megvalositja a toldalektalanitast
+        tok = stemmer.stem(tok)
+
+        # stop szo kiszedes az indexbol
+        if tok in stopword:
+            continue
+
+        if len(tok) < 2:
+            continue
+
+        index.add(tok)
+
+    return index
+
--- termnyelv/main.py
+++ termnyelv/main.py
@@ -1,24 +1,66 @@
-from termnyelv.index.indexSearch import find_index
+from termnyelv.index.indexSearch import find_index_number, find_index_words
 from termnyelv.pdf.importPdf import load_pdf
 
-if __name__ == "__main__":
-    pdf_direc = "C:\\Users\\fengx\\PycharmProjects\\termeszetesnyelv_hazi\\res"
+global_index = {}
+pdf_direc = "C:\\Users\\fengx\\PycharmProjects\\termeszetesnyelv_hazi\\res"
 
-    docs = load_pdf(pdf_direc)
+def index_build(docs):
+    for doc in docs:
+        filename = doc['filename']
 
-    #teszt dokumentum feldolgozasra
-#    for doc in docs:
-#        print(f"{doc['filename']}")
-#        for pagecont in doc['pages']:
-#            print(f"Page {pagecont['page_number']}:")
-#            print(f"Content: {pagecont['text']}")
+        for pagecont in doc['pages']:
+            #hol van az oldalon
+            place = f"{filename} (page: {pagecont['page_number']})"
 
-    #teszt indexkereses
-    word = "az"
+            words = find_index_words(pagecont)
+
+            for word in words:
+                if word not in global_index:
+                    global_index[word] = set()
+                global_index[word].add(place)
+
+
+def search_global(word):
+    word = word.lower()
+    if word in global_index:
+        print(f"The '{word}' word is in the documents:")
+        for loc in global_index[word]:
+            print(f"  - {loc}")
+    else:
+        print(f"Can not find '{word}' in documents.")
+
+
+
+
+def doc_list(docs):
+    for doc in docs:
+        print(f"{doc['filename']}")
+        for pagecont in doc['pages']:
+            print(f"Page {pagecont['page_number']}:")
+            print(f"Content: {pagecont['text']}")
+
+
+#teszt indexkeresdarabszammal
+#ebbol egy listat kene csinalni nem mindig mindenegyes alkalommal lefutni
+#last global_index
+def search_doc_num(word,docs):
+    word = word.lower()
     for doc in docs:
         print(doc['filename'])
         for pagecont in doc['pages']:
-            index = find_index(pagecont)
-            #vagy fuggvenyben vagy kint??
+            index = find_index_number(pagecont)
+             #vagy fuggvenyben vagy kint??
             if word in index:
                 print(f"Word founded: {word} in {pagecont['page_number']} times: {index[word]}")
+
+if __name__ == "__main__":
+
+    docs = load_pdf(pdf_direc)
+
+    #felepiti az indexet
+    index_build(docs)
+
+    #teszt dokumentum feldolgozasra
+    #doclist(docs)
+    search_global("nyelv")
+    #search_doc_num("Nyelv",docs)
